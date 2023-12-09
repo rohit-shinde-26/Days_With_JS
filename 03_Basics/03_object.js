d1280f8a057421e6736902c5cbd5c1e5e08b4aa5
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2023-12-09 10:32:39 +0530

    object basic

---
+++ 03_Basics/03_object.js
@@ -0,0 +1,39 @@
+// singleton
+// object.create
+// object literals
+
+const mysym = Symbol("key1")  //defined the symbol
+
+
+const JsUser = {
+    name : "Rohit",
+    "Full Name" : "Rohit Sanjay Shinde",
+    age : 20,
+    location: "Sangamner",
+    email : "jagduaegfd",
+    isLoggedIn : false ,
+    lastLoginDays : ["Monday", "Sturday"],
+    [mysym] : "mykey1",  // add symbol in object
+}
+
+console.log(JsUser.email);
+console.log(JsUser["Full Name"]);  // required if the condition is in string
+console.log(JsUser[mysym]);
+
+JsUser.email = "ram@google.com";
+// Object.freeze(JsUser)  // after freeze object cant change 
+JsUser.email = "abhi@microsoft.com"
+console.log(JsUser);
+
+
+// define function
+JsUser.msg = function(){
+    console.log("hello its me");
+
+}
+JsUser.msgtwo = function(){
+    console.log(`hello its me, ${this.name}`);
+}
+console.log(JsUser.msg);  // undefined
+console.log(JsUser.msg());  
+console.log(JsUser.msgtwo());  
