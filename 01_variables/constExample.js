9c7bf0483e4d346023078f6f129dfeaae7135206
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2023-12-25 11:24:28 +0530

    normal changes

--- 01_variables/constExample.js
+++
@@ -1,8 +0,0 @@
-const Id = 4568977;
-let name = "Ram";
-var password = "54464651";
-let states;  //undefined
-
-// Id = 5646546; // Not alloweed
-console.log(Id);
-console.table([Id,name,password,states])
--- 01_variables/variables.js
+++
@@ -1,8 +0,0 @@
-const a = 26;
-const b = 30;
-
-let c = a + b;
-console.log(c);
-
-let x = "Sita" + " " + "Ram";  // for space
-console.log(x);
