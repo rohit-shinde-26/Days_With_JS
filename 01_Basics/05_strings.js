0babb8bf5e2caaef10b0bb9d8f0a929e9f25500c
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-01-23 11:16:40 +0530

    file has update with some details

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "Rohit"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('Rohit-S-Shinde')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
