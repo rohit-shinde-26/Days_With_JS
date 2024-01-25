ffb56b90658c839f65222ea604afa99d8c2106a7
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-01-25 11:09:15 +0530

    comparison has updated

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -8,14 +8,19 @@
 // console.log("2" > 1);
 // console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+
+// avoid the following comparison
+console.log(null > 0);  // false
+console.log(null == 0);  // false
+console.log(null >= 0);  // true
 
 console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
+// all false
+
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2);  // false
+// it checks value as well as its datatype strictly
