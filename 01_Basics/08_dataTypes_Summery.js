d405f86730a554bd12b9fe5327d5533e07cb67e3
Rohit-shinde <100394511+rohit-shinde-26@users.noreply.github.com>
2024-01-25 11:27:33 +0530

    datatype summery will be ready

--- 01_Basics/08_dataTypes_Summery.js
+++ 01_Basics/08_dataTypes_Summery.js
@@ -1,28 +1,35 @@
 //  Primitive
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+// it is call by value
 
+
+
+// it dont have special datatype for . values
 const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn = false
-const outsideTemp = null
-let userEmail;
+const outsideTemp = null  // it completely empty
+let userEmail;   // undefined
+
+
 
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+console.log(id === anotherId);  // false : written value are different
 
-// const bigNumber = 3456543576654356754n
+// const bigNumber = 3456543576654356754n //n for bigint
 
 
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
-
+// all reference typeof is object
 const heros = ["shaktiman", "naagraj", "doga"];
+
 let myObj = {
     name: "hitesh",
     age: 22,
@@ -32,6 +39,13 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
+
+// it is used for seeing datatype of any id
 console.log(typeof anotherId);
 
+
+// always typeof null is object
+
+
+// see the following documentation
 // https://262.ecma-international.org/5.1/#sec-11.4.3
